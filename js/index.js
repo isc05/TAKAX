4d08991843f143bf08f42d3a2be9d1c0a10aaa96
GarianV <abraham.villanueva.angel.u@gmail.com>
2025-10-12 13:21:45 -0600

    link fix > index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 if(!user){
-    window.location.href = 'login.html'
+    window.location.href = '../login.html';
 }
 
 const logout = document.querySelector('#logout')
@@ -8,5 +8,5 @@ const logout = document.querySelector('#logout')
 logout.addEventListener('click', ()=>{
     alert('Hasta pronto!')
     localStorage.removeItem('login_success')
-    window.location.href = 'login.html'
-})
+    window.location.href = '../login.html';
+});
